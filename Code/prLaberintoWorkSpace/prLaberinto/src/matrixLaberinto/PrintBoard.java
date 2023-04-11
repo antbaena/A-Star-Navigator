4d9fed82f3adf893d12218f3e622cf57d4556655
tbms18 <124881416+tbms18@users.noreply.github.com>
2023-04-11 11:50:53 +0200

    printboard

--- Code/prLaberintoWorkSpace/prLaberinto/src/matrixLaberinto/PrintBoard.java
+++ Code/prLaberintoWorkSpace/prLaberinto/src/matrixLaberinto/PrintBoard.java
@@ -6,12 +6,9 @@ public class PrintBoard {
     public static void printBoard(Board board){
         for(int i = 0; i < board.FILAS; i++){
             for(int j = 0; j < board.COLUMNAS; j++){
-
+                System.out.print(board.matrix[i][j] + " ");
             }
+            System.out.println("");
         }
-    }
-
-    private static void printNode(Nodo n){
-        
     }
 }
