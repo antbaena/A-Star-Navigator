9c76aa057f9a3f4a64dbd929b2e3e9ef401f70c2
tbms18 <124881416+tbms18@users.noreply.github.com>
2023-04-11 12:28:25 +0200

    ALRIGHT SUPERGRASS

--- Code/prLaberintoWorkSpace/prLaberinto/src/MainTest.java
+++ Code/prLaberintoWorkSpace/prLaberinto/src/MainTest.java
@@ -4,9 +4,7 @@ public class MainTest {
 
 	public static void main(String[] args) {
 		Board lab = new Board();
-		lab.GenerarObstaculos();
 		PrintBoard.printBoard(lab);
-		
 		lab.getCasillaFinal();
 		lab.getCasillaInicial();
 	}
