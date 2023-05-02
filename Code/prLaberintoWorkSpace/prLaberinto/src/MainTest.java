8bc94251b3ed045a9e3905be5c5988fb795ac784
antBaena <antbaena@uma.es>
2023-05-02 10:43:25 +0200

    aaaaaaaaa

--- Code/prLaberintoWorkSpace/prLaberinto/src/MainTest.java
+++ Code/prLaberintoWorkSpace/prLaberinto/src/MainTest.java
@@ -9,34 +9,17 @@ import nodo.Nodo;
 public class MainTest {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		Board lab = new Board(0.5f);
-=======
-<<<<<<< Updated upstream
-		Board lab = new Board(0);
->>>>>>> 1246de81cf90a828bb4799ad7a4001e48b760766
-		//PrintBoard.printBoard(lab);
-		//PrintBoard.printBoardHeruistica(lab);
-		int caminoFin = new AStarAlgorithm().buscarCamino(lab.getCasillaInicial());
-		if(caminoFin == 0) {
-			System.err.println("Camino no Disponible");
-			PrintBoard.printBoard(lab);
-			System.err.println("Camino no Disponible");
-		} else {
-			PrintBoard.printBoardFile(lab,"output.txt");
-			PrintBoard.printBoard(lab);
-=======
 		System.out.println("Prueba con 0% de obstaculos");
+		
 		for(int i = 0; i < 100; i++) {
 			Board lab = new Board(0);
-			boolean caminoFin = new AStarAlgorithm().buscarCamino(lab.getCasillaInicial());
-			if(caminoFin == false) {
+			int caminoFin = new AStarAlgorithm().buscarCamino(lab.getCasillaInicial());
+			if(caminoFin == 0) {
 				System.err.println("Camino no Disponible");
 			} else {
 				PrintBoard.printBoardFile(lab,"output.txt");
 				PrintBoard.printBoard(lab);
 			}
->>>>>>> Stashed changes
 		}
 	}
 
